fbe3123bf21a9365ef0d05f3ab5cec2a621dd89b
shiwenjie <shiwenjie@chinadaas.com>
2019-12-10 14:42:56 +0800

    初始化

---
+++ src/main/java/com/mine/monitor/MineMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.mine.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MineMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MineMonitorApplication.class, args);
+    }
+
+}
